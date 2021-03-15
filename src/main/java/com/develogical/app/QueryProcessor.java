70b90a1249f17b82bdaee838a4b3237d1b90e599
Edward Poduthas <epoduthas@bell-integration.com>
2021-03-15 14:22:16 +0000

    query changed

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("newton")) {
+            return "Sir Isaac Newton PRS (25 December 1642 – 20 March 1726) was an English mathematician, " +
+                    "physicist, astronomer, theologian, and author (described in his time as a natural philosopher) " +
+                    "who is widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution.";
+        }
         return "";
     }
 }
